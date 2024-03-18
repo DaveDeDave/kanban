1b8212078e750c0086255374e5a4fdddc7c85220
Davide Ceci <davide.ceci@enhancers.it>
2024-03-18 01:11:57 +0100

    Remove old database utility

--- packages/backend/libs/base/src/database/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./develop.database";
-export * from "./mongo.database";
--- packages/backend/libs/base/src/index.ts
+++ packages/backend/libs/base/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./authentication";
 export * from "./exceptions";
-export * from "./database";
 export * from "./utility";
 export * from "./schemas";
